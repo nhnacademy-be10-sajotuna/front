7aabd107832f79a1c7744f3b3c267109ecf92ca1
flashdragon <yooh3574@naver.com>
2025-06-18 17:41:22 +0900

    [feat] : 필터 테스트
    - 필터 테스트

--- src/main/java/shop/nhnteam04/front/accountFeignClient/AccountFeignClient.java
+++ src/main/java/shop/nhnteam04/front/accountFeignClient/AccountFeignClient.java
@@ -1,10 +1,7 @@
 package shop.nhnteam04.front.accountFeignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import shop.nhnteam04.front.user.request.LoginRequestUser;
 import shop.nhnteam04.front.user.response.LoginResponse;
 
@@ -14,5 +11,5 @@ public interface AccountFeignClient {
     public LoginResponse login(@RequestBody LoginRequestUser loginRequestUser);
 
     @GetMapping("/api/users/me")
-    public void me();
+    public void me(@RequestHeader("Cookie") String cookieHeader);
 }
