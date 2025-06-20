41d86f2e60a9d7386bd3f80f3c7dae34fac0f035
flashdragon <yooh3574@naver.com>
2025-06-20 18:05:13 +0900

    [feat] : 롤백후
    - 브라우저의 진입점을 게이트웨이로 만들고 많은것들 수정
    - aop 삭제, 토큰 만료시 account-api로 직접 feign을 날려 요청 받도록 교체

---
+++ src/main/java/shop/nhnteam04/front/controller/TokenController.java
@@ -0,0 +1,49 @@
+package shop.nhnteam04.front.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import shop.nhnteam04.front.feign.account.AccountFeignClient;
+import shop.nhnteam04.front.service.CookieService;
+import shop.nhnteam04.front.user.response.ResponseAccessToken;
+
+@Controller
+@RequiredArgsConstructor
+public class TokenController {
+
+    private final AccountFeignClient accountFeignClient;
+    private final CookieService cookieService;
+
+    @GetMapping("/token/refresh")
+    public String refresh(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("refresh_token")) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (refreshToken == null) {
+            return "/login";
+        }
+        try {
+            String bearToken = "Bearer " + refreshToken;
+            ResponseAccessToken accessToken = accountFeignClient.refreshToken(bearToken);
+            ResponseCookie cookie = cookieService.getAccessTokenCookie(accessToken.getAccessToken());
+            response.addHeader("Set-Cookie", cookie.toString());
+        } catch (Exception e) {
+            return "/login";
+        }
+
+        String originalUrl = request.getHeader("Referer");
+        if (originalUrl == null) originalUrl = "/"; // 기본 홈으로
+
+        return "redirect:" + originalUrl;
+    }
+}
