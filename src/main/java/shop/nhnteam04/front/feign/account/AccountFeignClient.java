4c7600bca420b0f591549cb5eb0a1608b76fb832
flashdragon <yooh3574@naver.com>
2025-06-24 15:51:59 +0900

    [feat] : 주소
    - 주소 관리 form생성
    - 주소 추가
    - 주소 삭제
    - 주소 조회

--- src/main/java/shop/nhnteam04/front/feign/account/AccountFeignClient.java
+++ src/main/java/shop/nhnteam04/front/feign/account/AccountFeignClient.java
@@ -2,11 +2,15 @@ package shop.nhnteam04.front.feign.account;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+import shop.nhnteam04.front.address.request.RequestAddress;
+import shop.nhnteam04.front.address.response.ResponseAddress;
 import shop.nhnteam04.front.user.request.LoginRequestUser;
 import shop.nhnteam04.front.user.request.RegisterRequestUser;
 import shop.nhnteam04.front.user.response.LoginResponse;
 import shop.nhnteam04.front.user.response.ResponseAccessToken;
 import shop.nhnteam04.front.user.response.ResponseUserWithPolicy;
+
+import java.util.List;
 
 @FeignClient(name= "account-api")
 public interface AccountFeignClient {
@@ -27,4 +31,13 @@ public interface AccountFeignClient {
 
     @DeleteMapping("/api/users/me")
     public void deleteUser(@RequestHeader("X-User-Id") Long userId);
+
+    @GetMapping("/api/address")
+    public List<ResponseAddress> getAddresses(@RequestHeader("X-User-Id") Long userId);
+
+    @PostMapping("/api/address")
+    public void saveAddress(@RequestBody RequestAddress address, @RequestHeader("X-User-Id") Long userId);
+
+    @DeleteMapping("/api/address/{addressId}")
+    public void deleteAddress(@RequestHeader("X-User-Id") Long userId, @PathVariable("addressId") Long addressId);
 }
