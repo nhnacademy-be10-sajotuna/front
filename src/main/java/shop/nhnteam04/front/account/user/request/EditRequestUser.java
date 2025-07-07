79d78f997e4b1741d6ed6dc0438a90a0c472a0c7
flashdragon <yooh3574@naver.com>
2025-07-07 10:08:29 +0900

    [feat] : front valid
    - 비밀번호는 변경 안해도 됨

--- src/main/java/shop/nhnteam04/front/account/user/request/EditRequestUser.java
+++ src/main/java/shop/nhnteam04/front/account/user/request/EditRequestUser.java
@@ -16,7 +16,6 @@ public class EditRequestUser {
     @NotNull(message = "이름은 비어 있으면 안됩니다.")
     @Size(min = 1, max = 10, message = "이름은 1자리 이상 10자리 이하 입니다.")
     private String name;
-    @Size(min = 3, message = "비밀번호는 3자리 이상입니다.")
     private String password;
     @NotNull(message = "번호는 비어 있으면 안됩니다.")
     private String phoneNumber;
