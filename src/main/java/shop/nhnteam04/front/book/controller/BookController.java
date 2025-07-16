9ec1727226754b84b7db9c476e0b3956dabb17b5
flashdragon <yooh3574@naver.com>
2025-07-16 17:25:09 +0900

    [feat] : book detail - 책 상세 페이지 - 쿠폰 받아오는 기능 - 장바구니 담는기능

---
+++ src/main/java/shop/nhnteam04/front/book/controller/BookController.java
@@ -0,0 +1,44 @@
+package shop.nhnteam04.front.book.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import shop.nhnteam04.front.account.user.dto.SecurityUser;
+import shop.nhnteam04.front.book.dto.response.BookResponse;
+import shop.nhnteam04.front.book.dto.response.CategoryResponse;
+import shop.nhnteam04.front.book.service.BookService;
+import shop.nhnteam04.front.review.response.ReviewResponse;
+import shop.nhnteam04.front.review.service.ReviewService;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+    private final ReviewService reviewService;
+
+
+    @GetMapping("/{book-isbn}")
+    public String book(@AuthenticationPrincipal SecurityUser user, @PathVariable("book-isbn") String isbn, Model model) {
+        BookResponse book = bookService.getBookByIsbn(isbn);
+
+        List<ReviewResponse> reviews = reviewService.getReviewsByBook(isbn);
+        model.addAttribute("reviews", reviews);
+        model.addAttribute("isbn", isbn);
+        model.addAttribute("user", user);
+        model.addAttribute("book", book);
+        Set<Long> categoryIds = book.getCategories().stream().flatMap(List::stream).map(CategoryResponse::getId).collect(Collectors.toSet());
+        model.addAttribute("categoryIds", categoryIds);
+        return "book/book";
+    }
+}
