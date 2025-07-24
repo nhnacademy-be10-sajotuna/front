4768b8e10971af60b2a75b87a63d57159534760f
flashdragon <yooh3574@naver.com>
2025-07-24 14:37:03 +0900

    [feat] : 스프링 캐쉬 - 카테고리에 스프링 캐쉬 적용 - 조회시 레디스에 적용 - 추가 및 삭제시 레디스에서 카테고리 삭제

--- src/main/java/shop/nhnteam04/front/config/RedisConfig.java
+++ src/main/java/shop/nhnteam04/front/config/RedisConfig.java
@@ -2,9 +2,12 @@ package shop.nhnteam04.front.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -18,6 +21,20 @@ public class RedisConfig {
         sessionRedisTemplate.setHashKeySerializer(new StringRedisSerializer());
         sessionRedisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return sessionRedisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
 
